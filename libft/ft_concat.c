357742a0456bd516434c1a327958a27b8d8070a8
hsilverbage <henriksilverbage@gmail.com>
2024-07-03 12:00:00 +0200

    📄 Add libft/ft_concat.c

---
+++ libft/ft_concat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_concat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 14:11:11 by hdamitzi          #+#    #+#             */
+/*   Updated: 2023/09/08 14:12:08 by hdamitzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_concat(char *s1, char *s2)
+{
+	char	*dst;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	dst = (char *)ft_calloc((len_s1 + len_s2 + 1), sizeof(*dst));
+	if (dst == NULL)
+		return (NULL);
+	ft_memcpy(dst, s1, len_s1);
+	ft_memcpy((dst + len_s1), s2, len_s2);
+	free((void *)s1);
+	return (dst);
+}
