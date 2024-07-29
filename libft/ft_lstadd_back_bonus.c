590f73666651075b3e1fe40c706c2997672731f8
hsilverbage <henriksilverbage@gmail.com>
2024-07-29 12:00:00 +0200

    📄 Add libft/ft_lstadd_back_bonus.c

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 11:52:40 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/21 11:52:43 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst)
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+	else if (!*lst)
+		*lst = new;
+}
