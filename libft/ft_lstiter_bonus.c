e89b260cd6d254363fb1f06815a6b23e97250313
hsilverbage <henriksilverbage@gmail.com>
2024-08-06 12:00:00 +0200

    📄 Add libft/ft_lstiter_bonus.c

---
+++ libft/ft_lstiter_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 12:13:26 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/21 12:13:28 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	if (!lst || !f)
+		return ;
+	temp = lst;
+	while (lst)
+	{
+		temp = lst->next;
+		f(lst->content);
+		lst = temp;
+	}
+}
