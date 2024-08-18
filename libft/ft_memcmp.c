2823f028de41a0d5999f92ff76855ca29b69cd19
hsilverbage <henriksilverbage@gmail.com>
2024-08-18 12:00:00 +0200

    📄 Add libft/ft_memcmp.c

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 15:19:18 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/14 15:19:20 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*str1;
+	char	*str2;
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	return (0);
+}
