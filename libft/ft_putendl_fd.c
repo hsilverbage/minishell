a78d5c171b1c716191da364220a144fe99149735
hsilverbage <henriksilverbage@gmail.com>
2024-09-03 12:00:00 +0200

    📄 Add libft/ft_putendl_fd.c

---
+++ libft/ft_putendl_fd.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 13:00:48 by hdamitzi          #+#    #+#             */
+/*   Updated: 2023/08/21 22:45:44 by hdamitzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <fcntl.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(s, "\n");
+	ft_putstr_fd(tmp, fd);
+	free(tmp);
+}
