4cb1094024ca26b0dccdedd4cc0dc40eebe4adb4
hsilverbage <henriksilverbage@gmail.com>
2024-09-17 12:00:00 +0200

    📄 Add libft/ft_strchr.c

---
+++ libft/ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 13:52:16 by hdamitzi          #+#    #+#             */
+/*   Updated: 2022/11/11 13:52:19 by hdamitzi         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == (unsigned char)c)
+		return ((char *)(s + i));
+	return (0);
+}
