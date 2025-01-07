692c82c42a4fda532fe051202aff69abf9867937
hsilverbage <henriksilverbage@gmail.com>
2025-01-07 12:00:00 +0100

    📄 Add srcs/signal/handle_signal.c

---
+++ srcs/signal/handle_signal.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_signal.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 18:33:07 by hdamitzi          #+#    #+#             */
+/*   Updated: 2023/09/08 09:15:16 by hdamitzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	signal_reset_prompt(int signo)
+{
+	(void)signo;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	set_signals_interactive(void)
+{
+	struct sigaction	act;
+
+	ft_memset(&act, 0, sizeof(act));
+	ignore_sigquit();
+	act.sa_handler = &signal_reset_prompt;
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	signal_print_newline(int signal)
+{
+	(void)signal;
+	rl_on_new_line();
+}
+
+void	set_signals_noninteractive(void)
+{
+	struct sigaction	act;
+
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_handler = &signal_print_newline;
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGQUIT, &act, NULL);
+}
+
+void	ignore_sigquit(void)
+{
+	struct sigaction	action;
+
+	ft_memset(&action, 0, sizeof(action));
+	action.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &action, NULL);
+}
